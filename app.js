279a029bae3548348afb47f03f0a515c6deed2a3
LinhDong64 <mailinhdong@gmail.com>
2020-08-06 15:07:58 -0700

    tạo thư mục models, contents, vwAccount. Tạo view sign up

--- app.js
+++ app.js
@@ -6,12 +6,17 @@ app.engine('hbs', exphbs({
     defaultLayout: 'main.hbs',
     layoutsDir: 'views/_layouts'
 }));
+app.use(express.static("contents"));
 
 app.set('view engine', 'hbs');
  
 app.get('/', function (req, res) {
   res.render('home');
 });
+
+app.get('/signup', (req, res)=>{
+  res.render("vwAccount/signup");
+})
 
 app.get('/login', (req, res)=>{
   res.render("vwAccount/login");
