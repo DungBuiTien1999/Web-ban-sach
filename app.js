6b5e73cc1cc933bc7dafc03593bda2020360b4a6
DungBuiTien1999 <44043622+DungBuiTien1999@users.noreply.github.com>
2020-08-08 10:04:52 +0700

    tao trang chu version 1.0.0

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const exphbs  = require('express-handlebars');
+const express_handlebars_sections = require('express-handlebars-sections');
+// const session = require('express-session');
+const path = require('path');
 
 const app = express();
+
+// app.use(session({
+//   secret: 'keyboard cat',
+//   resave: false,
+//   saveUninitialized: true,
+//   // cookie: {
+//   //     secure: true
+//   // }
+// }))
+app.use(express.static(path.join(__dirname, '/Contents')));
 app.engine('hbs', exphbs({
     defaultLayout: 'main.hbs',
-    layoutsDir: 'views/_layouts'
+    layoutsDir: 'views/_layouts',
+    helpers: {
+      section: express_handlebars_sections()
+    }
 }));
 
 app.set('view engine', 'hbs');
